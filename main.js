409a67c4c5106dafe9e5984b9e4473ccd59bbe5f
cenrich <ceci_enrich@hotmail.com>
2019-08-12 19:48:40 -0300

    minimal changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-fetch('https://reqres.in/api/user')
+fetch('https://raw.githubusercontent.com/cenrich/fetch/master/cv.json')
     .then(response => response.json())
     .then(res => {
-        // console.log(res.data);
-        const {data} = res
-        data.map(({id, name}) => console.log(`id: ${id}\nname: ${name}`))
+        console.log(res);
+        // const {data} = res
+        // data.map(({id, name}) => console.log(`id: ${id}\nname: ${name}`))
     })
